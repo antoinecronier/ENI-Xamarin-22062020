1ed3905efaab0cbac0bede5d339a33a6cfc7fda0
harckline <antoinecronier1@gmail.com>
2020-07-02 17:09:36 +0200

    patch android listview rows

--- Tp9/Tp9.Web/Startup.cs
+++ Tp9/Tp9.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
